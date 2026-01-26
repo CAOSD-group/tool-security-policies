12cebf2c524488a366a2e1d09d0013342ad48a5e
Brian Flores Lopez <bfl699@uma.es>
2026-01-26 21:01:34 +0100

    Updates folders and new source of kyverno policies

--- scripts/auxs/copy_valid_confs.py
+++ scripts/auxs/copy_valid_confs.py
@@ -20,8 +20,8 @@ valid_filenames = []
 with open(csv_path, newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-      if row["Valid"].strip() == "True":    # filtrar solo True
-          valid_filenames.append(row["Filename"].strip())
+        if row["Valid"].strip() == "True":    # filtrar solo True
+            valid_filenames.append(row["Filename"].strip())
 
 print(f"Total archivos marcados como True: {len(valid_filenames)}")
 
@@ -29,14 +29,14 @@ copiados = 0
 
 # Recorrer y copiar los archivos válidos
 for fname in valid_filenames:
-  src = os.path.join(source_folder, fname)
-  dst = os.path.join(destination_folder, fname)
+    src = os.path.join(source_folder, fname)
+    dst = os.path.join(destination_folder, fname)
 
-  if os.path.exists(src):
-      shutil.copy2(src, dst)   # copia manteniendo metadata
-      copiados += 1
-  else:
-      print(f"⚠ Archivo no encontrado: {src}")
+    if os.path.exists(src):
+        shutil.copy2(src, dst)   # copia manteniendo metadata
+        copiados += 1
+    else:
+        print(f"⚠ Archivo no encontrado: {src}")
 
 print(f"Archivos copiados correctamente: {copiados}")
 print("Proceso completado.")
