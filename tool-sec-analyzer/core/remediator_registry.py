077f123fb043f441f8c8c311bfe2a9dd547c65d7
NOMBRE <CORREO@example.com>
2026-03-17 17:59:32 +0100

    New changes of the project: adding features for modules and version stable of 'remediations'

--- tool-sec-analyzer/core/remediator_registry.py
+++ tool-sec-analyzer/core/remediator_registry.py
@@ -9,7 +9,43 @@ class RemediationRegistry:
             "PodDisruptionBudget_maxUnavailable_Non_Zero": [{"feature_to_fix": "io_k8s_api_policy_v1_PodDisruptionBudget_spec_maxUnavailable_asInteger", "safe_value": 1}],
             "Require_Multiple_Replicas": [{"feature_to_fix": "io_k8s_api_apps_v1_Deployment_spec_replicas", "safe_value": 2}],
             "insecureCapabilities": [{"feature_to_fix": "io_k8s_api_core_v1_Container_securityContext_capabilities_drop_StringValue", "safe_value": "ALL"}],
-            "Restrict_Service_Port_Range": [{"feature_to_fix": "io_k8s_api_core_v1_Service_spec_ports_port", "safe_value": 32500}]
+            "Restrict_Service_Port_Range": [{"feature_to_fix": "io_k8s_api_core_v1_Service_spec_ports_port", "safe_value": 32400}],
+            # --- Network & Services ---
+            #"Restrict_Service_Port_Range": [{"feature_to_fix": "io_k8s_api_core_v1_Service_spec_ports_port", "safe_value": 32000}],
+            "Disallow_Localhost_ExternalName_Services": [{"feature_to_fix": "io_k8s_api_core_v1_Service_spec_externalName", "safe_value": "development.local"}],            
+            # --- Workload & Pods ---
+            "Require_Multiple_Replicas": [{"feature_to_fix": "io_k8s_api_apps_v1_Deployment_spec_replicas", "safe_value": 2}],
+            #"Disallow_Default_Namespace": [{"feature_to_fix": "io_k8s_api_core_v1_Pod_metadata_namespace", "safe_value": "kube-system"}],
+            "PodDisruptionBudget_maxUnavailable_Non_Zero": [{"feature_to_fix": "io_k8s_api_policy_v1_PodDisruptionBudget_spec_maxUnavailable_asInteger", "safe_value": 1}],
+            "hpaMinAvailability": [{"feature_to_fix": "io_k8s_api_autoscaling_v1_HorizontalPodAutoscaler_spec_minReplicas", "safe_value": 3}],
+            #"hpaMaxAvailability": [{"feature_to_fix": "io_k8s_api_autoscaling_v1_HorizontalPodAutoscaler_spec_maxReplicas", "safe_value": 10}],
+            
+            # --- Security Context, Users & Groups ---
+            #"insecureCapabilities": [{"feature_to_fix": "io_k8s_api_core_v1_Container_securityContext_capabilities_drop_StringValue", "safe_value": "ALL"}],
+            #"Require_Run_As_Non_Root_User": [{"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_securityContext_runAsUser_valueInt", "safe_value": 1000}],
+            #"use_high_uid": [{"feature_to_fix": "io_k8s_api_core_v1_Container_securityContext_runAsUser_valueInt", "safe_value": 10001}],
+            #"use_high_gid": [{"feature_to_fix": "io_k8s_api_core_v1_Container_securityContext_runAsGroup_valueInt", "safe_value": 10001}],
+            "Check_supplementalGroups": [{"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_securityContext_supplementalGroups_IntegerValue", "safe_value": 100}],
+            "Require_Non_Root_Groups": [ {"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_securityContext_supplementalGroups_IntegerValue", "safe_value": 100}, {"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_securityContext_fsGroup_valueInt", "safe_value": 2000}],
+            #"Validate_User_ID_Group_ID_and_FS_Group": [ {"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_securityContext_runAsUser_valueInt", "safe_value": 1000}, {"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_securityContext_runAsGroup_valueInt", "safe_value": 3000}, {"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_securityContext_fsGroup_valueInt", "safe_value": 2000}],
+            
+            # --- Storage & Volumes ---
+            "Require_StorageClass": [{"feature_to_fix": "io_k8s_api_core_v1_PersistentVolumeClaim_spec_storageClassName", "safe_value": "standard"}],
+            "Restrict_StorageClass": [{"feature_to_fix": "io_k8s_api_storage_v1_StorageClass_reclaimPolicy", "safe_value": "Delete"}],
+            "Limit_hostPath_PersistentVolumes_to_Specific_Directories": [{"feature_to_fix": "io_k8s_api_core_v1_PersistentVolume_spec_hostPath_path", "safe_value": "/data/secure"}],
+            "Enforce_ReadWriteOncePod": [{"feature_to_fix": "io_k8s_api_core_v1_PersistentVolumeClaim_spec_accessModes_StringValue", "safe_value": "ReadWriteOncePod"}],
+            
+            # --- RBAC & Authentication ---
+            "Restrict_Binding_to_Cluster_Admin": [{"feature_to_fix": "io_k8s_api_rbac_v1_RoleBinding_roleRef_name", "safe_value": "view"}],
+            "Restrict_Binding_System_Groups": [{"feature_to_fix": "io_k8s_api_rbac_v1_RoleBinding_subjects_name", "safe_value": "safe-user"}],
+            "Deny_Secret_Service_Account_Token_Type": [{"feature_to_fix": "io_k8s_api_core_v1_Secret_type", "safe_value": "Opaque"}],
+            #"Require_aws_node_DaemonSet_use_IRSA": [{"feature_to_fix": "io_k8s_api_apps_v1_DaemonSet_spec_template_spec_serviceAccountName", "safe_value": "irsa-sa"}],
+            
+            # --- Regex Fallbacks (Valores seguros para inyectar si falla la validación léxica) ---
+            #"Check_Environment_Variables": [{"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_containers_env_value", "safe_value": "false"}],
+            "Require_Pod_priorityClassName": [{"feature_to_fix": "io_k8s_api_core_v1_Pod_spec_priorityClassName", "safe_value": "high-priority"}],
+            #"Restrict_Image_Registries": [{"feature_to_fix": "io_k8s_api_core_v1_Container_image", "safe_value": "eu.foo.io/segura:v1"}],
+            "tagNotSpecified": [{"feature_to_fix": "io_k8s_api_core_v1_Container_image", "safe_value": "nginx:v1.28.0"}],
         }
         self._build_registry_from_uvl(uvl_path)
 
